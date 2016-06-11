934ee780b087b68b1beca54d836116d022b67ccd
AnkurSarode <ankur.sarode@gmail.com>
2016-06-11 16:06:38 +0530

    Solved issues on homepage

--- static/js/script.js
+++ static/js/script.js
@@ -28,7 +28,7 @@
           </div>");
         });
 
-      $(".homepage-add-projects-button").css("top",Wht);
+
       if(Wht<Wwt)
       {
         $(".hello-image-center").css("height",Wht*1.1/3);
@@ -55,7 +55,6 @@
     $(".homepage-carousel").css("top",Wht/4.5);
 
     $(".cards").css("top",Wht/10);
-    $(".footer").css("height",Wht/3);
     $(".profile-other-user").css("height",Wht/10);
 
     var Onboarding_navHt=$("#onboarding-nav").height()
@@ -65,70 +64,11 @@
   if(Wwt>Wht)
     {
       $(".homepage-image-active-carousel-image").css("height",Wht/1.64);
-      if(Wht>1000)
-      {
-        var card_height=Wht/6;
-        $(".customcard").css("height",card_height);
-        $(".homepage-add-projects-button").css("margin-top","15%");
-        $(".cards").css("top",Wht/6);
-
-        if(Wwt<1430 && Wwt>1000)
-        {
-          var card_height=Wht/5;
-          $(".customcard").css("height",card_height);
-          $(".footer").css("height",Wht/2.5);
-        }
-        else if(Wwt<1700 && Wwt>1400)
-        {
-          var card_height=Wht/5.5;
-          $(".customcard").css("height",card_height);
-        }
-      }
-      else
-      {
-        var card_height=Wht/3;
-        $(".customcard").css("height",card_height);
-        $(".footer").css("height",Wht/2.5);
-      }
-      if(Wwt>2000)
-      {
-        var card_height=Wht/5;
-        $(".customcard").css("height",card_height);
-      }
     }
       else
       {
         $(".homepage-image-active-carousel-image").css("height",Wht/2.5);
         $(".homepage-image-active-carousel-image").css("width",Wwt);
-        $(".footer").css("height",Wht/2.2);
-        $(".homepage-add-projects-button").css("margin-top","50%");
-        $(".cards").css("top",Wht/5);
-        $(".homepage-carousel").css("top",Wht/3);
-        if(Wwt>700)
-        {
-          var card_height=Wht/6;
-          $(".customcard").css("height",card_height);
-        }
-        else if(Wwt>413)
-        {
-          var card_height=Wht/3.4;
-          $(".customcard").css("height",card_height);
-        }
-        else if(Wwt>330)
-        {
-          var card_height=Wht/3;
-          $(".customcard").css("height",card_height);
-        }
-        else if(Wwt<330)
-        {
-          var card_height=Wht/3;
-          $(".customcard").css("height",card_height);
-        }
-        else
-        {
-          var card_height=Wht/3.6;
-          $(".customcard").css("height",card_height);
-        }
       }
        $(".card-footer").css("top",card_height-50);
       /*Fixing the profile or profile_other*/
@@ -232,9 +172,8 @@ $(window).resize(function()
 
     }
     $(".homepage-carousel").css("top",Wht/4.5);
-    $(".homepage-add-projects-button").css("margin-top","10%");
+
     $(".cards").css("top",Wht/10);
-    $(".footer").css("height",Wht/3);
 
     var Onboarding_navHt=$("#onboarding-nav").height()
     $(".onboarding-carousel-control").css("top",Onboarding_navHt/2);
@@ -243,71 +182,15 @@ $(window).resize(function()
     if(Wwt>Wht)
     {
       $(".homepage-image-active-carousel-image").css("height",Wht/1.64);
-      if(Wht>1000)
-      {
-        var card_height=Wht/6;
-        $(".customcard").css("height",card_height);
-        $(".homepage-add-projects-button").css("margin-top","15%");
-        $(".cards").css("top",Wht/6);
-        $(".profile-other-user").css("height",Wht/8);
-        if(Wwt<1430 && Wwt>1000)
-        {
-          var card_height=Wht/5;
-          $(".customcard").css("height",card_height);
-          $(".footer").css("height",Wht/2.5);
-        }
-        else if(Wwt<1700 && Wwt>1400)
-        {
-          var card_height=Wht/5.5;
-          $(".customcard").css("height",card_height);
-        }
-      }
-      else
-      {
-        var card_height=Wht/3;
-        $(".customcard").css("height",card_height);
-        $(".footer").css("height",Wht/2.5);
-      }
-      if(Wwt>2000)
-      {
-        var card_height=Wht/5;
-        $(".customcard").css("height",card_height);
-      }
 
     }
       else
       {
         $(".homepage-image-active-carousel-image").css("height",Wht/2.5);
         $(".homepage-image-active-carousel-image").css("width",Wwt);
-        $(".footer").css("height",Wht/2.2);
-        $(".homepage-add-projects-button").css("margin-top","50%");
+
         $(".cards").css("top",Wht/5);
         $(".homepage-carousel").css("top",Wht/3);
-        if(Wwt>700)
-        {
-          var card_height=Wht/6;
-          $(".customcard").css("height",card_height);
-        }
-        else if(Wwt>413)
-        {
-          var card_height=Wht/3.4;
-          $(".customcard").css("height",card_height);
-        }
-        else if(Wwt>330)
-        {
-          var card_height=Wht/3;
-          $(".customcard").css("height",card_height);
-        }
-        else if(Wwt<330)
-        {
-          var card_height=Wht/3;
-          $(".customcard").css("height",card_height);
-        }
-        else
-        {
-          var card_height=Wht/3.6;
-          $(".customcard").css("height",card_height);
-        }
       }
 
       $(".card-footer").css("top",card_height-50);
